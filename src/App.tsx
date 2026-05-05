5139a3d807152763d0528c0003c4ea397772eecc
ajaishanth-sg <ajaishanth22@gmail.com>
2026-05-05 11:11:19 +0530

    Chore: Configure project for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/portfoilio'}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
